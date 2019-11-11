1e6c705a3951aebb3752c1c3a8d1da85ccec23dd
Paul <isakura313@gmail.com>
2019-11-12 01:06:07 +0300

    LOL

--- code.js
+++ code.js
@@ -6,24 +6,15 @@
 
 /* 
 класс ITEMDEAL
-нужноп переписать  саму генерацию класса
-
 */
 
-class ItemDeal{
-    constructor(name, color, now){
-        this.name = name;
-        this.color = color; 
-        this.now = new Date();
-    }
+class ItemDeal {
+  constructor(name, color, now) {
+    this.name = name;
+    this.color = color;
+    this.now = new Date();
+  }
 }
-
-// просто пример создания
-let item  = new ItemDeal('prikol', 'red'); // здесь скорее всего можно по человечески переписать конструктор
-console.dir(item); // просто тестовый прогон
-console.log(+item.now);
-
-
 
 
 let select = document.querySelector('#important');
@@ -44,142 +35,127 @@ let deals = document.querySelector('.deals');
 //IA = important array
 let IA = ['has-text-danger', 'has-text-warning', 'has-text-success'];
 let Animation_Array = ['bounceOut', 'rollOut', 'rotateOut', 'lightSpeedOut'];
-let Month_Array = [ 'Январь', 'Февраль ', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+let Month_Array = ['Января', 'Февраля ', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
 
 
 
 //здесь у нас происходит первичная прорисовка приложения
 
 
-(function drawOnLoad(){
-    for (let i = 0; i < localStorage.length; i++) {
-        let lk_key = localStorage.key(i);
-        let content = localStorage.getItem(lk_key);
-        let parse_content = JSON.parse(content);
-        // console.dir(parse_content);
-        // console.log(parse_content.name);
-        // console.log(parse_content.color);
-        // console.log(parse_content.now.getDate());
-        //здесь у нас пойдет сама отрисовочка
-        let dat = Date.parse(parse_content.now);
-        dat  = new Date(dat)
-        console.warn(dat.getDate());
-
-        deals.insertAdjacentHTML('afterbegin', 
-        `<div class='wrap-task' id ="${parse_content.id}">
-            <div class="task is-size-4">
-            <p> <span class="${IA[parse_content.color]}">  ${parse_content.name}  </span>  ${dat.getDate()} ${Month_Array[dat.getMonth()]} ${dat.getFullYear()} </p>
-            </div>
-        <span class="icon is-large tr">
-        <i class="fas fa-trash-alt trash"></i>
-        </span>
-        </div>`);
-      field.value = '';
-    }
+(function drawOnLoad() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let lk_key = localStorage.key(i);
+    let content = localStorage.getItem(lk_key);
+    let parse_content = JSON.parse(content);
+    let dat = Date.parse(parse_content.now);
+    parse_content.now = new Date(dat);
+    GenerateDom(deals, parse_content);
+  }
 })();
 
 // дальше у нас пойдет сама отрисовочка
 
 function createItem() {
-    let text = field.value; // получение данных из input
-    if (!text) {
-      return;
-      //return сразу прекращает выполнение функции
-    }
-    let item = new ItemDeal("",`${text}`, `${select.value - 1}`);
-    let lk_id = item.id;
-    let myJson = JSON.stringify(item);
-    localStorage.setItem(lk_id, myJson);
-    deals.insertAdjacentHTML('afterbegin', 
-        `<div class="wrap-task animated zoomInLeft" id = "${item.id}">
-        <div class="task is-size-4 ${IA[select.value - 1]}">
-            <p>${text} ${item.now.getDate()} ${Month_Array[item.now.getMonth()]} ${item.now.getFullYear()} </p>
-            </div>
-        <span class="icon is-large tr">
-            <i class="fas fa-trash-alt trash"></i>
-        </span>
-  </div>`);
-    field.value = '';
+  let text = field.value; // получение данных из input
+  if (!text) {
+    return;
   }
-  
-  
-  
-  button.addEventListener('click', createItem);
+  let item = new ItemDeal(`${text}`, `${select.value - 1}`);
+  let myJson = JSON.stringify(item);
+  localStorage.setItem(+item.now, myJson);
+  GenerateDom(deals, item);
+}
 
-  document.addEventListener('keypress', (event) => {
-    if (event.keyCode == 13) {
-      createItem();
-    }
-  });
+
+
+button.addEventListener('click', createItem);
+
+document.addEventListener('keypress', (event) => {
+  if (event.keyCode == 13) {
+    createItem();
+  }
+});
 
 
 // удаление элемента
 deals.addEventListener('click', function (event) {
-    let item = event.target.closest('i');
-    let item2 = event.target.closest('.wrap-task');
-    if (!item || !deals.contains(item)) {
-      return;
-    }
-  
-    item2.className = `animated ${Animation_Array[1]} wrap-task`;
-    setTimeout(function () {
-      // item2.parentNode.removeChild(item2);// это можно переписать проще
-      item2.remove();
-      let id_element = item2.id;
-      // x = x.trim();
-      //трим вырезает лишние пробелы
-      console.log(id_element);
-      localStorage.removeItem(id_element);
-    }, 700)
-  });
-  
+  let item = event.target.closest('i');
+  let item2 = event.target.closest('.wrap-task');
+  if (!item || !deals.contains(item)) {
+    return;
+  }
 
-/* часть связанная с мотивацией
-игрока 
-*/
+  item2.className = `animated ${Animation_Array[GR(Animation_Array)]} wrap-task`;
+  setTimeout(() => {
+    item2.remove();
+    localStorage.removeItem(item2.id);
+  }, 700)
+});
 
 
-
-  let motivation_array = [
-    'Двигайся вперед и никогда не сдавайся',
-    'Расти большой не будь лапшой',
-    'Just do it',
-    'Лучший в мире за работой'
+let motivation_array = [
+  'Двигайся вперед и никогда не сдавайся',
+  'Расти большой не будь лапшой',
+  'Just do it',
+  'Лучший в мире за работой'
 ];
 
-  function changePhrase() {
-    document.querySelector('.MotSpeech').innerHTML = motivation_array[Math.round(Math.random() * (motivation_array.length - 1))];
-    document.querySelector('.MotSpeech').className = "MotSpeech anime";
-  }
-  //запуск смены фраз;
-  setInterval(changePhrase, 8000);
+function changePhrase() {
+  document.querySelector('.MotSpeech').innerHTML = motivation_array[GR(motivation_array)];
+  document.querySelector('.MotSpeech').className = "MotSpeech anime";
+}
+
+//эта функция занимается генерацией различных цифер
+function GR(arr) {
+  return Math.round(Math.random() * (arr.length - 1))
+}
+
+//запуск смены фраз;
+setInterval(changePhrase, 8000);
 
 
 
-  // если выбрана 1  то красное is-danger 
+// если выбрана 1  то красное is-danger 
 // если выбрана 2  то красное is-warning 
 // если выбрана 3  то красное is-success 
 // здесь у нас должна быть отрисовка  
 
-  function ChangeColorSelect(val){
-    switch (val) {
-      case 1:
-        select.className = 'is-danger ';
-        break;
-      case 2:
-          select.className = 'is-danger ';
-        break;
-      case 3:
-          select.className = 'is-danger ';
-        break;
-      
-    
-      default:
-        break;
-    }
+function ChangeColorSelect(val) {
+  switch (val) {
+    case '1':
+      select.className = 'has-background-danger has-text-white';
+      break;
+    case '2':
+      select.className = 'has-background-link has-text-white';
+      break;
+    case '3':
+      select.className = 'has-background-primary has-text-white';
+      break;
+    default:
+      alert("wtf");
+      select.className = 'has-text-danger';
+      break;
   }
+}
 
-  ChangeColorSelect(select);
+window.onload = () =>{
+  ChangeColorSelect(select.value);
+}
+select.onchange = () =>{
+  ChangeColorSelect(select.value);
+}
 
+// отдельная функция на генерацию DOM
+function GenerateDom(aimElem, obj) {
+  aimElem.insertAdjacentHTML('afterbegin',
+    `<div class="wrap-task animated zoomInLeft" id="${+obj.now}">
+  <div class="task is-size-4">
+      <p> <span class=${IA[select.value - 1]}> ${obj.name} </span> ${obj.now.getDate()} ${Month_Array[obj.now.getMonth()]} ${obj.now.getFullYear()} </p>
+      </div>
+      <span class="icon is-large tr">
+      <i class="fas fa-trash-alt trash"></i>
+  </span>
+</div>`);
+  field.value = '';
+}
 
-  //сюда я хочу заложить отдельную функцию не генерацию и парсинг времени
